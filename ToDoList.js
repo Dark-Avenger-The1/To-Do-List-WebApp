f3d5b085c762fe78c79a2608e4b6c97d7cf1208e
Jasper Sam <jaspersam824@gmail.com>
2026-01-15 23:43:31 +0800

    Complete the basic javascript logic for To List

--- ToDoList.js
+++ ToDoList.js
@@ -1,18 +1,60 @@
-const list = ["Conquer Britain","Set Sail to Atlantic"];
+let quest = ["Conquer Britain","Set Sail to Atlantic"];
 let btnAdd = document.querySelector(".btnAdd");
-
-
+let questList = document.querySelector('.ulQuestList');
+let txtQuest = document.querySelector('.txtQuest');
+let btnClear = document.querySelector('.btnClear');
+loadQuest();
 btnAdd.onclick = () =>{
     addQuest();
 };
+btnClear.onclick = () => {
+    clearData();
+};
 
 function addQuest(){
-    let quest = document.querySelector(".txtQuest").text;
-    list.add(quest);
+    let questName = txtQuest.value; // Get the text typed in the box
+    if (questName === "") {
+        alert("Enter a Quest");
+       return;
+    }else if(quest.includes(questName)){
+        alert(questName+" Already Exist");
+        return;
+    }else{
+         quest.push(questName); // Use .push() for arrays
+         // Clear the box after adding
+         txtQuest.value="";
+        loadQuest(); // Refresh the list on screen
+    }
 }
 
 function loadQuest(){
-    for(let i =0;i<list.length;i++){
-        
+    questList.innerHTML="";
+    let accumaltor ="";
+    for(let i =0;i<quest.length;i++){
+        accumaltor=quest[i];
+        const li = document.createElement("li");
+        li.textContent=accumaltor;
+        li.append(createButton(i));
+        questList.append(li);
     }
 }
+
+function createButton(index){
+    const button = document.createElement('button');
+    button.className="btnQuestComplete"
+    button.innerText="X";
+    button.onclick = () =>{
+        list.splice(index,1);
+        loadQuest();
+    };
+    return button;
+}
+
+function clearData(){
+    alert("Are you sure to abandon all the quest?");
+    questList.innerHTML="";
+    quest=[];
+    loadQuest();
+}
+
+
