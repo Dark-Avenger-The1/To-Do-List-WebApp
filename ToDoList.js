d6e362878c8c467257b3fd9a87103d7dfc4e8c28
Jasper Sam <jaspersam824@gmail.com>
2026-01-15 21:29:51 +0800

    Modefied some files

--- ToDoList.js
+++ ToDoList.js
@@ -1,1 +1,18 @@
-const list = ["Jogging","Buy Minute Burger"];
+const list = ["Conquer Britain","Set Sail to Atlantic"];
+let btnAdd = document.querySelector(".btnAdd");
+
+
+btnAdd.onclick = () =>{
+    addQuest();
+};
+
+function addQuest(){
+    let quest = document.querySelector(".txtQuest").text;
+    list.add(quest);
+}
+
+function loadQuest(){
+    for(let i =0;i<list.length;i++){
+        
+    }
+}
